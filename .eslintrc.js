0707a8947284cac9153d74902fee81612e30335e
JIN <jinjiting2690@gmail.com>
2021-06-03 10:20:23 +0800

    chore: init project

---
+++ .eslintrc.js
@@ -0,0 +1,6 @@
+module.exports = {
+  extends: ['airbnb', 'airbnb/hooks'],
+  rules: {
+    semi: ['error', 'never'],
+  },
+}
---
+++ stylelint.js
@@ -0,0 +1,17 @@
+module.exports = {
+  extends: ['stylelint-config-standard', 'stylelint-config-rational-order'],
+  plugins: ['stylelint-declaration-block-no-ignored-properties'],
+  rules: {
+    'function-url-quotes': 'always',
+    // iconfont
+    'font-family-no-missing-generic-family-keyword': null,
+    'plugin/declaration-block-no-ignored-properties': true,
+    'unit-no-unknown': [
+      true,
+      {
+        ignoreUnits: ['rpx'],
+      },
+    ],
+  },
+  ignoreFiles: ['**/*.js', '**/*.jsx', '**/*.tsx', '**/*.ts'],
+}
