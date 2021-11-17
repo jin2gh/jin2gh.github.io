12377d23e621f7b683725960aacf93c724d474b7
JIN <jinjiting2690@gmail.com>
2021-11-17 13:55:47 +0800

    Updates

---
+++ component---src-pages-404-jsx-20c224b548214a383ada.js
@@ -0,0 +1,2 @@
+"use strict";(self.webpackChunkjinjiting_github_io=self.webpackChunkjinjiting_github_io||[]).push([[256],{70:function(t,e,n){n.r(e);var u=n(496);e.default=function(){return u.createElement("div",null,u.createElement("h1",null,"404: Not Found"),u.createElement("p",null,"You just hit a route that doesn't exist... the sadness."))}}}]);
+//# sourceMappingURL=component---src-pages-404-jsx-20c224b548214a383ada.js.map
