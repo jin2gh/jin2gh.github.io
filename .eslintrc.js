3fc88520f2b54d9ad4a7be50c6bc063a7b5a981d
jin <jinjiting2690@gmail.com>
2022-08-06 21:19:12 +0800

    chore(config): add eslint-config-ruler

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,12 +1,11 @@
 module.exports = {
-  parser: 'babel-eslint',
-  extends: ['airbnb', 'airbnb/hooks'],
+  parser: [
+    '@babel/eslint-parser',
+    // '@babel/preset-react'
+  ],
+  extends: [
+    'ruler',
+    'ruler/react',
+  ],
   plugins: [],
-  rules: {
-    semi: [2, 'never'], // 分号
-    'arrow-parens': [2, 'as-needed'], // 参数括号
-    'jsx-a11y/click-events-have-key-events': 0,
-    'jsx-a11y/no-static-element-interactions': 0,
-    'no-param-reassign': 0,
-  },
-}
+};
